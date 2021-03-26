56ee21c37d34d5d3627b01fe823c787c7c0d65c6
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-03-26 02:59:13 +0100

    V1.1 with sampling

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,6 +3,7 @@ import com.google.common.hash.Funnels;
 import gr.james.sampling.ChaoSampling;
 import gr.james.sampling.WeightedRandomSamplingCollector;
 import org.apache.commons.lang3.time.StopWatch;
+import org.semanticweb.yars.nx.Node;
 import org.semanticweb.yars.nx.parser.NxParser;
 
 import java.io.*;
@@ -19,16 +20,16 @@ public class Parser {
     public String rdfFile = "";
     public HashSet<String> classesHashSet = new HashSet<>();
     public HashMap<String, Integer> classInstanceCount = new HashMap<>();
-    HashMap<String, BloomFilter<String>> classInstanceBloomFilters = new HashMap<>();
     HashMap<String, HashSet<String>> classInstances = new HashMap<>();
-    HashMap<String, HashSet<String>> classProperties = new HashMap<>();
-    
     HashMap<String, Double> instanceFrequency = new HashMap<>();
     HashMap<String, HashMap<String, Double>> classInstanceWithFrequency = new HashMap<>();
-    HashMap<String, HashMap<String, HashSet<String>>> classPropWithType = new HashMap<>();
-    
+    HashMap<String, HashSet<String>> classProperties = new HashMap<>();
     HashMap<String, ArrayList<String>> sampledClassInstances = new HashMap<>();
+    HashMap<String, HashSet<String>> instanceProps = new HashMap<>();
     
+    HashMap<String, BloomFilter<String>> classInstanceBloomFilters = new HashMap<>();
+    
+    //HashMap<String, HashMap<String, HashSet<String>>> classPropWithType = new HashMap<>();
     Parser(String filePath) {
         this.rdfFile = filePath;
     }
@@ -39,7 +40,9 @@ public class Parser {
         try {
             FileInputStream rdf = new FileInputStream(rdfFile);
             NxParser nxp = new NxParser(rdf);
-            nxp.iterator().forEachRemaining(nodes -> {
+            
+            while (nxp.iterator().hasNext()) {
+                Node[] nodes = nxp.next();
                 //filter RDF.type triples
                 if (nodes[1].toString().equals("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>")) {
                     classesHashSet.add(nodes[2].toString());
@@ -62,25 +65,49 @@ public class Parser {
                 } else {
                     instanceFrequency.put(nodes[0].toString(), 1.0);
                 }
-            });
+            }
             
             //In case you want to sort the results
             this.classInstanceCount = classInstanceCount.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-            //this.instanceFrequency = instanceFrequency.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
             
             classInstanceCount.forEach((s, integer) -> {
                 System.out.println(s + " -> " + integer);
             });
-            System.out.println("Size: ");
-            System.out.println(classesHashSet.size());
-            //System.out.println(classesHashSet);
-            
+            System.out.println("Class Count: " + classesHashSet.size());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        
         watch.stop();
         System.out.println("Time Elapsed parseClasses: " + watch.getTime());
+    }
+    
+    public void parseInstanceProps() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        try {
+            FileInputStream rdf = new FileInputStream(rdfFile);
+            NxParser nxp = new NxParser(rdf);
+            
+            while (nxp.iterator().hasNext()) {
+                Node[] nodes = nxp.next();
+                
+                if (instanceProps.containsKey(nodes[0].toString())) {
+                    //HashSet<String> prop = instanceProps.get(nodes[0].toString());
+                    instanceProps.get(nodes[0].toString()).add(nodes[1].toString());
+                    //prop.add(nodes[1].toString());
+                    //instanceProps.put(nodes[0].toString(), prop);
+                } else {
+                    HashSet<String> prop = new HashSet<>();
+                    prop.add(nodes[1].toString());
+                    instanceProps.put(nodes[0].toString(), prop);
+                }
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        watch.stop();
+        System.out.println("Time Elapsed parseInstanceProps: " + watch.getTime());
     }
     
     public void mapInstanceFrequency() {
@@ -101,81 +128,59 @@ public class Parser {
                 }
             });
         });
+        
+        // sort them based on out degree in descending order
+        /* classInstanceWithFrequency.entrySet().stream().forEach(hm -> {
+            hm.setValue(hm.getValue().entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new)));
+        });*/
+        /* classInstanceWithFrequency.entrySet().stream().forEach(hm -> {
+            System.out.println(hm.getKey() + " -> \n" + hm.getValue());
+        });*/
         watch.stop();
         System.out.println("Time Elapsed mapInstanceFrequency: " + watch.getTime());
     }
     
     public void performSampling() {
-        classInstanceWithFrequency.forEach( (k, v) -> {
-            WeightedRandomSamplingCollector<String> collector = ChaoSampling.weightedCollector( (v.size() * 20)/100, new Random());
-            ArrayList<String> sample = (ArrayList<String>) instanceFrequency.entrySet().stream().collect(collector);
-            sampledClassInstances.put(k, sample);
-        });
-    }
-    
-    public void initHasMap() {
         StopWatch watch = new StopWatch();
         watch.start();
-        classInstanceCount.forEach((s, integer) -> {
-            //classInstances.put(s, new HashSet<>());
-            classProperties.put(s, new HashSet<>());
-            classPropWithType.put(s, new HashMap<>());
-            
+        classInstanceWithFrequency.entrySet().parallelStream().forEach(hashmap -> {
+            if (hashmap.getValue().size() > 1) {
+                WeightedRandomSamplingCollector<String> collector = ChaoSampling.weightedCollector((hashmap.getValue().size() * 20) / 100, new Random());
+                ArrayList<String> sample = (ArrayList<String>) hashmap.getValue().entrySet().stream().collect(collector);
+                sampledClassInstances.put(hashmap.getKey(), sample);
+            } else {
+                System.out.println("SKIP: Sample Size for " + hashmap.getKey() + " is less than 1.");
+            }
         });
         watch.stop();
-        System.out.println("Time Elapsed initHasMap: " + watch.getTime());
-        
+        System.out.println("Time Elapsed performSampling: " + watch.getTime());
     }
     
-    public void generateClassInstancesHashMaps() {
+    public void extractClassPropertiesByIteratingOverTheFile() {
         StopWatch watch = new StopWatch();
         watch.start();
         try {
-            FileInputStream file = new FileInputStream(rdfFile);
-            NxParser nxp = new NxParser(file);
-            nxp.iterator().forEachRemaining(nodes -> {
-                //filter class instances
-                if (classesHashSet.contains(nodes[2].toString())) {
-                    HashSet<String> h = classInstances.get(nodes[2].toString());
-                    h.add(nodes[0].toString());
-                    classInstances.put(nodes[2].toString(), h);
-                }
+            //init hashMap
+            classesHashSet.forEach(c -> {
+                classProperties.put(c, new HashSet<>());
             });
             
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        watch.stop();
-        System.out.println("Time Elapsed generateClassInstancesHashMaps: " + watch.getTime());
-    }
-    
-    /**
-     * This method extracts properties of each class and store in a HashSet
-     * LUBM - 91 Million triples, 23 classes - 4.3 minutes on local mac
-     */
-    
-    public void extractClassProperties() {
-        StopWatch watch = new StopWatch();
-        watch.start();
-        int counter = 0;
-        try {
             FileInputStream file = new FileInputStream(rdfFile);
             NxParser nxp = new NxParser(file);
-            //HashMap<String, List<String>> instances = new HashMap<>();
-            nxp.iterator().forEachRemaining(nodes -> {
+            while (nxp.iterator().hasNext()) {
+                Node[] nodes = nxp.next();
                 
-                classInstances.forEach((c, instancesHashSet) -> {
-                    if (instancesHashSet.contains(nodes[0].toString())) {
-                        if (classPropWithType.get(c).get(nodes[1].toString()) != null) {
-                            HashSet<String> temp = classPropWithType.get(c).get(nodes[1].toString());
-                            temp.addAll(findObjectType(nodes[2].toString()));
-                            classPropWithType.get(c).put(nodes[1].toString(), temp);
-                        } else {
-                            classPropWithType.get(c).put(nodes[1].toString(), findObjectType(nodes[2].toString()));
-                        }
+                sampledClassInstances.forEach((c, props) -> {
+                    
+                    if (props.contains(nodes[0].toString())) {
+                        HashSet<String> l = classProperties.get(c);
+                        l.add(nodes[1].toString());
+                        classProperties.put(c, l);
                     }
+                    
                 });
-            });
+                
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -184,38 +189,43 @@ public class Parser {
         System.out.println("Time Elapsed extractClassProperties: " + watch.getTime());
     }
     
-    
-    private HashSet<String> findObjectType(String object) {
-        HashSet<String> objectTypes = new HashSet<>();
-        classInstanceBloomFilters.forEach((c, bf) -> {
-            // check for type of property, i.e., object
-            if (bf.mightContain(object)) {
-                //System.out.println("Type detected for " + object + " -> " + c);
-                objectTypes.add(c);
-            }
+    public void extractClasses() {
+        //init hashMap
+        classesHashSet.forEach(c -> {
+            classProperties.put(c, new HashSet<>());
         });
-        return objectTypes;
+        sampledClassInstances.forEach((c, instances) -> {
+            instances.parallelStream().forEach(instance -> {
+                if (instanceProps.containsKey(instance)) {
+                    classProperties.get(c).addAll(instanceProps.get(instance));
+                }
+            });
+            
+        });
     }
-    
- 
     
     public static void main(String[] args) throws Exception {
         String filePath = args[0];
         Parser parser = new Parser(filePath);
         parser.parseClasses();
-        parser.mapInstanceFrequency();
-        parser.performSampling();
+        parser.parseInstanceProps();
+        //parser.mapInstanceFrequency();
+        //parser.performSampling();
         
         System.out.println("______________________________________________________________________");
-        parser.classInstances.forEach((k,v) -> {
+        parser.classInstances.forEach((k, v) -> {
             System.out.println(k + " -> " + v.size());
         });
         
         System.out.println("______________________________________________________________________");
-        parser.sampledClassInstances.forEach((k,v) -> {
+        parser.sampledClassInstances.forEach((k, v) -> {
             System.out.println(k + " -> " + v.size());
         });
         
+        //parser.extractClassProperties();
         
+        System.out.println("______________________________________________________________________");
+        //parser.classProperties.entrySet().parallelStream().forEach(System.out::println);
+        //parser.instanceProps.entrySet().parallelStream().forEach(System.out::println);
     } // main ends here
 }
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -8,33 +8,80 @@ import java.nio.charset.StandardCharsets;
 
 public class Utils {
 
-//    public void initBloomFilters() {
-//        StopWatch watch = new StopWatch();
-//        watch.start();
-//        classInstanceCount.forEach((s, integer) -> {
-//            System.out.println(s + " -> " + integer);
-//            classInstanceBloomFilters.put(s, BloomFilter.create(Funnels.stringFunnel(StandardCharsets.UTF_8), integer, 0.01));
-//        });
-//    }
-//
-//    public void generateBFs() {
-//        StopWatch watch = new StopWatch();
-//        watch.start();
-//        try {
-//            FileInputStream file = new FileInputStream(rdfFile);
-//            NxParser nxp = new NxParser(file);
-//            nxp.iterator().forEachRemaining(nodes -> {
-//                //filter class instances
-//                if (classesHashSet.contains(nodes[2].toString())) {
-//                    //nodes[0] is the instance
-//                    classInstanceBloomFilters.get(nodes[2].toString()).put(nodes[0].toString());
-//                }
-//            });
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        watch.stop();
-//        System.out.println("Time Elapsed generateBFs: " + watch.getTime());
-//    }
+/*    public void initBloomFilters() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        classInstanceCount.forEach((s, integer) -> {
+            System.out.println(s + " -> " + integer);
+            classInstanceBloomFilters.put(s, BloomFilter.create(Funnels.stringFunnel(StandardCharsets.UTF_8), integer, 0.01));
+        });
+    }*/
+    
+/*    public void generateBFs() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        try {
+            FileInputStream file = new FileInputStream(rdfFile);
+            NxParser nxp = new NxParser(file);
+            nxp.iterator().forEachRemaining(nodes -> {
+                //filter class instances
+                if (classesHashSet.contains(nodes[2].toString())) {
+                    //nodes[0] is the instance
+                    classInstanceBloomFilters.get(nodes[2].toString()).put(nodes[0].toString());
+                }
+            });
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        watch.stop();
+        System.out.println("Time Elapsed generateBFs: " + watch.getTime());
+    }*/
+    
+        /*    public void initHasMap() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        classInstanceCount.forEach((s, integer) -> {
+            classProperties.put(s, new HashSet<>());
+            classPropWithType.put(s, new HashMap<>());
+        });
+        watch.stop();
+        System.out.println("Time Elapsed initHasMap: " + watch.getTime());
+    }*/
+    
+    /*    public void generateClassInstancesHashMaps() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        try {
+            FileInputStream file = new FileInputStream(rdfFile);
+            NxParser nxp = new NxParser(file);
+            nxp.iterator().forEachRemaining(nodes -> {
+                //filter class instances
+                if (classesHashSet.contains(nodes[2].toString())) {
+                    HashSet<String> h = classInstances.get(nodes[2].toString());
+                    h.add(nodes[0].toString());
+                    classInstances.put(nodes[2].toString(), h);
+                }
+            });
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        watch.stop();
+        System.out.println("Time Elapsed generateClassInstancesHashMaps: " + watch.getTime());
+    }*/
+    
+    
+    /*    private HashSet<String> findObjectType(String object) {
+        HashSet<String> objectTypes = new HashSet<>();
+        classInstanceBloomFilters.forEach((c, bf) -> {
+            // check for type of property, i.e., object
+            if (bf.mightContain(object)) {
+                //System.out.println("Type detected for " + object + " -> " + c);
+                objectTypes.add(c);
+            }
+        });
+        return objectTypes;
+    }*/
+    
 }
