e3beb622fbfd741f1d225d1e1702da1933196e2f
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-04-09 00:43:48 +0200

    Sampling Parser in action

--- src/main/java/SamplingParser.java
+++ src/main/java/SamplingParser.java
@@ -15,36 +15,42 @@ import java.util.stream.Collectors;
 import static java.util.Comparator.comparingInt;
 
 public class SamplingParser {
-
+    
     public String rdfFile = "";
+    public String samplingPercentage = "";
     SHACLER shacler = new SHACLER();
-
+    
     // Constants
     public final String RDFType = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>";
     public final String OntologyClass = "<http://www.w3.org/2002/07/owl#Ontology>";
-
+    
     // Classes, instances, properties
     HashMap<String, HashSet<String>> classToInstances = new HashMap<>();
     HashMap<String, HashMap<String, String>> classToPropWithObjType = new HashMap<>();
-
+    
     HashMap<String, String> instanceToClass = new HashMap<>();
     HashSet<String> properties = new HashSet<>();
     HashMap<String, HashSet<String>> propertyToTypes = new HashMap<>();
-
+    
     // Bloom Filters
     BloomFilter<CharSequence> subjObjBloomFilter = BloomFilter.create(Funnels.stringFunnel(StandardCharsets.UTF_8), 100_000_000, 0.01);
     BloomFilter<CharSequence> objPropTypeBloomFilter = BloomFilter.create(Funnels.stringFunnel(StandardCharsets.UTF_8), 100_000_000, 0.01);
-
+    
     // For Sampling Purpose
     HashMap<String, Double> instanceFrequency = new HashMap<>();
     HashMap<String, HashMap<String, Double>> classInstanceWithFrequency = new HashMap<>();
-    HashMap<String, ArrayList<String>> sampledClassInstances = new HashMap<>();
-
+    HashMap<String, ArrayList<String>> sampledClassToInstances = new HashMap<>();
+    
     // Constructor
     SamplingParser(String filePath) {
         this.rdfFile = filePath;
     }
-
+    
+    SamplingParser(String filePath, String samplingPercentage) {
+        this.rdfFile = filePath;
+        this.samplingPercentage = samplingPercentage;
+    }
+    
     public void firstPass() {
         StopWatch watch = new StopWatch();
         watch.start();
@@ -52,7 +58,7 @@ public class SamplingParser {
             Files.lines(Path.of(rdfFile))                           // - Stream of lines ~ Stream <String>
                     .forEach(line -> {                              // - A terminal operation
                         String[] nodes = line.split(" ");     // - Parse a <subject> <predicate> <object> string
-
+                        
                         if (nodes[1].contains(RDFType)) {
                             //Track instances per class
                             if (classToInstances.containsKey(nodes[2])) {
@@ -61,7 +67,7 @@ public class SamplingParser {
                                 HashSet<String> cti = new HashSet<String>() {{ add(nodes[0]); }};
                                 classToInstances.put(nodes[2], cti);
                             }
-
+                            
                             // Track classes per instance
                             instanceToClass.put(nodes[0], nodes[2]);
                         }
@@ -76,7 +82,7 @@ public class SamplingParser {
         watch.stop();
         System.out.println("Time Elapsed firstPass: " + TimeUnit.MILLISECONDS.toSeconds(watch.getTime()));
     }
-
+    
     public void secondPass() {
         StopWatch watch = new StopWatch();
         watch.start();
@@ -84,17 +90,17 @@ public class SamplingParser {
             properties.forEach(p -> {
                 propertyToTypes.put(p, new HashSet<>());
             });
-
+            
             Files.lines(Path.of(rdfFile))                           // - Stream of lines ~ Stream <String>
                     .filter(line -> !line.contains(RDFType))        // - Exclude RDF type triples
                     .forEach(line -> {                              // - A terminal operation
                         String[] nodes = line.split(" ");     // - Parse a <subject> <predicate> <object> string
-
+                        
                         //for this triple, I want to know, given a certain predicate, having its object, what is the type of its object, either it is literal or an IRI to some class
                         propertyToTypes.get(nodes[1]).add(instanceToClass.get(nodes[2]));
                         //  "<instance> <property> <type of the object>"
                         objPropTypeBloomFilter.put(nodes[0] + nodes[1] + instanceToClass.get(nodes[2]));
-
+                        
                         // Track frequency of instances for sampling purpose
                         if (instanceFrequency.containsKey(nodes[0].toString())) {
                             instanceFrequency.put(nodes[0].toString(), instanceFrequency.get(nodes[0].toString()) + 1);
@@ -102,14 +108,14 @@ public class SamplingParser {
                             instanceFrequency.put(nodes[0].toString(), 1.0);
                         }
                     });
-
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
         watch.stop();
         System.out.println("Time Elapsed secondPass: " + TimeUnit.MILLISECONDS.toSeconds(watch.getTime()));
     }
-
+    
     public void prioritizeClasses() {
         StopWatch watch = new StopWatch();
         watch.start();
@@ -117,25 +123,24 @@ public class SamplingParser {
                 .stream()
                 .sorted(comparingInt(e -> e.getValue().size()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-
+        
         watch.stop();
         System.out.println("Time Elapsed prioritizeClasses: " + TimeUnit.MILLISECONDS.toSeconds(watch.getTime()));
     }
-
+    
     public void propsExtractor() {
         StopWatch watch = new StopWatch();
         watch.start();
-
-        classToInstances.entrySet().forEach((classInstances -> {
+        
+        sampledClassToInstances.entrySet().forEach((classInstances -> {
             System.out.print("Class: " + classInstances.getKey() + ". No. of Instances: " + classInstances.getValue().size() + " ... ");
             StopWatch innerWatch = new StopWatch();
             innerWatch.start();
-
             classInstances.getValue().forEach(instance -> {
                 //instance is a <subject> string, the key is its type like subj rdf:type key
                 properties.forEach(p -> {
                     if (subjObjBloomFilter.mightContain(instance + p)) {
-
+                        
                         if (classToPropWithObjType.containsKey(classInstances.getKey())) {
                             // in case the property have more than one type of objects
                             if (propertyToTypes.get(p).size() > 1) {
@@ -147,7 +152,7 @@ public class SamplingParser {
                                     }
                                 });
                                 classToPropWithObjType.get(classInstances.getKey()).put(p, objTypes.toString());
-
+                                
                             } else {
                                 //only one type
                                 classToPropWithObjType.get(classInstances.getKey()).put(p, propertyToTypes.get(p).toString());
@@ -162,13 +167,13 @@ public class SamplingParser {
             });
             shacler.setParams(classInstances.getKey(), classToPropWithObjType.get(classInstances.getKey()));
             shacler.constructShape();
-
+            
             System.out.print(" Time Elapsed:" + TimeUnit.MILLISECONDS.toSeconds(innerWatch.getTime()) + " Sec\n");
         }));
         watch.stop();
         System.out.println("Time Elapsed propsExtractor: " + TimeUnit.MILLISECONDS.toSeconds(watch.getTime()));
     }
-
+    
     public void mapInstanceFrequency() {
         // Map the value of class Instances to their frequency in instanceFrequency
         StopWatch watch = new StopWatch();
@@ -187,26 +192,18 @@ public class SamplingParser {
                 }
             });
         });
-
-        // sort them based on out degree in descending order
-        /* classInstanceWithFrequency.entrySet().stream().forEach(hm -> {
-            hm.setValue(hm.getValue().entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new)));
-        });*/
-        /* classInstanceWithFrequency.entrySet().stream().forEach(hm -> {
-            System.out.println(hm.getKey() + " -> \n" + hm.getValue());
-        });*/
         watch.stop();
         System.out.println("Time Elapsed mapInstanceFrequency: " + watch.getTime());
     }
-
+    
     public void performSampling() {
         StopWatch watch = new StopWatch();
         watch.start();
         classInstanceWithFrequency.entrySet().parallelStream().forEach(hashmap -> {
             if (hashmap.getValue().size() > 1) {
-                WeightedRandomSamplingCollector<String> collector = ChaoSampling.weightedCollector((hashmap.getValue().size() * 2) / 100, new Random());
+                WeightedRandomSamplingCollector<String> collector = ChaoSampling.weightedCollector((hashmap.getValue().size() * Integer.parseInt(samplingPercentage)) / 100, new Random());
                 ArrayList<String> sample = (ArrayList<String>) hashmap.getValue().entrySet().stream().collect(collector);
-                sampledClassInstances.put(hashmap.getKey(), sample);
+                sampledClassToInstances.put(hashmap.getKey(), sample);
             } else {
                 System.out.println("SKIP: Sample Size for " + hashmap.getKey() + " is less than 1.");
             }
@@ -214,18 +211,19 @@ public class SamplingParser {
         watch.stop();
         System.out.println("Time Elapsed performSampling: " + watch.getTime());
     }
-
+    
     public static void main(String[] args) throws Exception {
         String filePath = args[0];
-        SamplingParser parser = new SamplingParser(filePath);
+        String samplingPercentage = args[1];
+        SamplingParser parser = new SamplingParser(filePath, samplingPercentage);
         parser.firstPass();
-
-        //parser.secondPass();
+        
         System.out.println("STATS: \n\t" + "No. of Classes: " + parser.classToInstances.size() + "\n\t" + "No. of distinct Properties: " + parser.properties.size());
-
-        //parser.prioritizeClasses();
-        //parser.propsExtractor();
-
+        parser.secondPass();
+        parser.mapInstanceFrequency();
+        parser.performSampling();
+        parser.propsExtractor();
+        
         System.out.println("*****");
         /*     parser.classToPropWithObjType.forEach((k, v) -> {
             System.out.println(k + " -> ");
@@ -234,11 +232,15 @@ public class SamplingParser {
             });
             System.out.println();
         });*/
-        //parser.shacler.printModel();
-
-
+        parser.shacler.printModel();
+        
+        
         SizeOf sizeOf = SizeOf.newInstance();
         System.out.println("Size - extras.Parser HashMap<String, HashSet<String>> classToInstances: " + sizeOf.deepSizeOf(parser.classToInstances));
+        System.out.println("Size - extras.Parser HashMap<String, Double> instanceFrequency: " + sizeOf.deepSizeOf(parser.instanceFrequency));
+        System.out.println("Size - extras.Parser HashMap<String, HashMap<String, Double>> classInstanceWithFrequency: " + sizeOf.deepSizeOf(parser.classInstanceWithFrequency));
+        System.out.println("Size - extras.Parser HashMap<String, ArrayList<String>> sampledClassToInstances: " + sizeOf.deepSizeOf(parser.sampledClassToInstances));
+        
         System.out.println("Size - extras.Parser HashMap<String, HashMap<String, String>> classToPropWithObjType: " + sizeOf.deepSizeOf(parser.classToPropWithObjType));
         System.out.println("Size - extras.Parser HashMap<String, String> instanceToClass: " + sizeOf.deepSizeOf(parser.instanceToClass));
         System.out.println("Size - extras.Parser HashSet<String> properties: " + sizeOf.deepSizeOf(parser.properties));
