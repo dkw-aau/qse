710c6f7a797d5a8838a1c022fa3f833ca1037f5e
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-05-10 15:28:33 +0200

    Wo cache

--- src/main/java/cs/Main.java
+++ src/main/java/cs/Main.java
@@ -40,11 +40,11 @@ public class Main {
         //BaselineWithART baselineWithART = new BaselineWithART(datasetPath, numberOfClasses);
         //baselineWithART.run();
         
-        //BaselineParserWithBloomFilters baselineParserWithBloomFilters = new BaselineParserWithBloomFilters(datasetPath, numberOfClasses);
-        //baselineParserWithBloomFilters.run();
+        BaselineParserWithBloomFilters baselineParserWithBloomFilters = new BaselineParserWithBloomFilters(datasetPath, numberOfClasses);
+        baselineParserWithBloomFilters.run();
         
-        BaselineParserWithBloomFilterCache baselineParserWithBloomFilterCache = new BaselineParserWithBloomFilterCache(datasetPath, numberOfClasses);
-        baselineParserWithBloomFilterCache.run();
+        //BaselineParserWithBloomFilterCache baselineParserWithBloomFilterCache = new BaselineParserWithBloomFilterCache(datasetPath, numberOfClasses);
+        //baselineParserWithBloomFilterCache.run();
         
     }
 }
