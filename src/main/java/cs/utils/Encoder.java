f352c4670a3fbf088d50662b66b03767f00c5878
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-05-11 12:39:37 +0200

    Fixed memory bug in encoder

--- src/main/java/cs/utils/Encoder.java
+++ src/main/java/cs/utils/Encoder.java
@@ -5,16 +5,23 @@ import java.util.HashMap;
 public class Encoder {
     int counter;
     HashMap<Integer, String> table;
+    HashMap<String, Integer> reverseTable;
     
     public Encoder() {
         this.counter = -1;
         this.table = new HashMap<>();
+        this.reverseTable = new HashMap<>();
     }
     
     public int encode(String val) {
-        counter++;
-        table.put(counter, val);
-        return counter;
+        if (reverseTable.containsKey(val)) {
+            return reverseTable.get(val);
+        } else {
+            this.counter++;
+            table.put(counter, val);
+            reverseTable.put(val, counter);
+            return counter;
+        }
     }
     
     public String decode(int val) {
