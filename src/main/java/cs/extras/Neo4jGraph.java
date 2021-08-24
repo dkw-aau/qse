8077a0816d2b2db41cd9a19e90b22094d291e5aa
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-08-24 15:55:31 +0200

    Neo4J visualizer methods

--- src/main/java/cs/extras/Neo4jGraph.java
+++ src/main/java/cs/extras/Neo4jGraph.java
@@ -1,25 +1,18 @@
 package cs.extras;
 
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import org.neo4j.driver.*;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import javax.ws.rs.core.MediaType;
 
 import static org.neo4j.driver.Values.parameters;
 
 public class Neo4jGraph {
     private static final String SERVER_ROOT_URI = "bolt://localhost:7687";
-    private Driver driver;
-    Node firstNode;
-    Node secondNode;
-    Relationship relationship;
+    private final Driver driver;
     
     public Neo4jGraph() {
         this.driver = GraphDatabase.driver(SERVER_ROOT_URI, AuthTokens.basic("neo4j", "12345"));
@@ -37,25 +30,8 @@ public class Neo4jGraph {
                     "  (a:Node),\n" +
                     "  (b:Node)\n" +
                     "WHERE a.value = $A AND b.value = $B\n" +
-                    "CREATE (a)-[r:RELTYPE {name: a.value + '<->' + b.value}]->(b)", parameters("A", source, "B", target));
+                    "CREATE (a)-[r:Relation {name: a.value + '<->' + b.value}]->(b)", parameters("A", source, "B", target));
         }
-    }
-    
-    public void addPerson(final String name) {
-        try (Session session = driver.session()) {
-            session.writeTransaction(new TransactionWork<Integer>() {
-                @Override
-                public Integer execute(Transaction tx) {
-                    
-                    return createPersonNode(tx, name);
-                }
-            });
-        }
-    }
-    
-    private static int createPersonNode(Transaction tx, String name) {
-        tx.run("CREATE (a:Person {name: $name})", parameters("name", name));
-        return 1;
     }
     
     public static void main(String[] args) throws URISyntaxException {
@@ -64,5 +40,4 @@ public class Neo4jGraph {
         neo.addNode(String.valueOf(2));
         neo.connectNodes(String.valueOf(1), String.valueOf(2));
     }
-    
 }
