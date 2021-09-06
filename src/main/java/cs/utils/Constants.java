c021db98993711dfc0be3a34c01afe50d23b9f80
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-09-06 13:38:58 +0200

    Constants

--- src/main/java/cs/utils/Constants.java
+++ src/main/java/cs/utils/Constants.java
@@ -3,6 +3,7 @@ package cs.utils;
 public class Constants {
     public static String MEMBERSHIP_GRAPH_ROOT_NODE = "<http://www.schema.hng.root> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.schema.hng.root#HNG_Root> .";
     public static String RDF_TYPE = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>";
+    public static String SUB_CLASS_OF = "<http://www.w3.org/2000/01/rdf-schema#subClassOf>";
     
     // some constant addresses of files
     public static String MG_VERTICES_FILE = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "_" + "mg_vertices.csv";
@@ -10,4 +11,5 @@ public class Constants {
     public static String MG_ENCODED_TABLE_FILE = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "_" + "mg_encoded_table.csv";
     public static String MG_ENCODED_R_TABLE_FILE = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "_" + "mg_encoded_table_reverse.csv";
     public static String FILTERED_DATASET = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "_" + "filtered.nt";
+    public static String SUBCLASSOF_DATASET = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "_" + "subclassOf.nt";
 }
