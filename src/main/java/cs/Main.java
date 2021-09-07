c88dbfb5355cb2b91e1c6a64ee3b19a64d83682f
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-09-07 16:38:32 +0200

    Order of execution changed

--- src/main/java/cs/Main.java
+++ src/main/java/cs/Main.java
@@ -19,12 +19,15 @@ public class Main {
         System.out.println("Baseline Algorithm A - Encoded Strings");
         new BaselineParserEncoded(datasetPath, numberOfClasses).run();
         
-        System.out.println("\n\n\n\nBaseline Algorithm B - Bloom Filters");
-        new BaselineParserWithBloomFilters(datasetPath, numberOfClasses).run();
-        
         System.out.println("\n\n\n\n Optimal Algorithm - Bloom Filters - BFS - Membership Graph");
         new MembershipGraphBasedParser(datasetPath, numberOfClasses).run();
-
+    
+    
+        System.out.println("\n\n\n\nBaseline Algorithm B - Bloom Filters");
+        new BaselineParserWithBloomFilters(datasetPath, numberOfClasses).run();
+    
+        
+        
         //new MembershipGraph(true);
         //new SmartTriplesFilterator(datasetPath).extractSubClassOfTriples();
         // If you want to extract Triples mapping to membership sets
