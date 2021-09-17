77c6489e21f75b8502d3e299162f36e571426d30
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-09-17 18:48:36 +0200

    Bloom Filter Bash Script Test

--- src/main/java/cs/Main.java
+++ src/main/java/cs/Main.java
@@ -7,6 +7,10 @@ import cs.parsers.mg.MgSchemaExtractor;
 import cs.parsers.mg.MgSchemaExtractorCache;
 import cs.utils.ConfigManager;
 import cs.utils.Utils;
+import orestes.bloomfilter.BloomFilter;
+import orestes.bloomfilter.FilterBuilder;
+
+import java.util.ArrayList;
 
 
 public class Main {
@@ -15,10 +19,14 @@ public class Main {
     public static int numberOfClasses;
     
     public static void main(String[] args) throws Exception {
-        configPath = args[0];
-        datasetPath = ConfigManager.getProperty("dataset_path");
-        numberOfClasses = Integer.parseInt(ConfigManager.getProperty("expected_number_classes"));
-        benchmark();
+        //configPath = args[0];
+        //datasetPath = ConfigManager.getProperty("dataset_path");
+        //numberOfClasses = Integer.parseInt(ConfigManager.getProperty("expected_number_classes"));
+        //benchmark();
+        ArrayList<BloomFilter> bloomFilterArrayList = new ArrayList<>();
+        for (int i = 0; i < Integer.parseInt(args[2]); i++) {
+            bloomFilterArrayList.add(new FilterBuilder(Integer.parseInt(args[0]), Double.parseDouble(args[1])).buildBloomFilter());
+        }
     }
     
     private static void benchmark() {
@@ -29,7 +37,7 @@ public class Main {
                 System.out.println("BlSchemaExtractor - Encoded Strings");
                 new BaselineParserEncoded(datasetPath, numberOfClasses).run();
             }
-    
+            
             if (isOn("WikiDataSchemaExtractor")) {
                 System.out.println("WikiDataSchemaExtractor - Encoded Strings");
                 new WikiDataBaselineParserEncoded(datasetPath, numberOfClasses).run();
@@ -61,10 +69,4 @@ public class Main {
     }
     
     private static boolean isOn(String option) {return Boolean.parseBoolean(ConfigManager.getProperty(option));}
-    
- /*   private static void test() {
-        for (int i = 0; i < Integer.parseInt(args[2]); i++) {
-            BloomFilter<String> bf = new FilterBuilder(Integer.parseInt(args[0]), Double.parseDouble(args[1])).buildBloomFilter();
-        }
-    }*/
 }
