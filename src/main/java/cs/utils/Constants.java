4e17b97824e7109b2b16ff0b30c99332aaaa7130
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-10-09 19:15:38 +0200

    Updated the address of the file to support dataset name

--- src/main/java/cs/utils/Constants.java
+++ src/main/java/cs/utils/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     public static String FILTERED_DATASET = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "_" + "filtered.nt";
     public static String SUBCLASSOF_DATASET = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "_" + "subclassOf.nt";
     
-    public static String TEMP_DATASET_FILE = ConfigManager.getProperty("output_file_path") + "/" + "shape-props-stats.csv";
+    public static String TEMP_DATASET_FILE = ConfigManager.getProperty("output_file_path") + "/" + ConfigManager.getProperty("dataset_name") + "-shape-props-stats.csv";
     public static String TEMP_DATASET_FILE_2 = ConfigManager.getProperty("output_file_path") + "/" + "shape-props-extended-stats.csv";
     public static String TEMP_DATASET_FILE_3 = ConfigManager.getProperty("output_file_path") + "/" + "shape-props-with-class-count-stats.csv";
     //
