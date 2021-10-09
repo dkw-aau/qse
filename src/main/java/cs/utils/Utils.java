9e5dc4dcfbb9342a23fae59919eb683005a9dc08
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-10-09 19:16:31 +0200

    Added Iri to Node converter feature

--- src/main/java/cs/utils/Utils.java
+++ src/main/java/cs/utils/Utils.java
@@ -3,9 +3,13 @@ package cs.utils;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.semanticweb.yars.nx.BNode;
+import org.semanticweb.yars.nx.Node;
+import org.semanticweb.yars.nx.Resource;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class Utils {
     
@@ -23,5 +27,13 @@ public class Utils {
     public static IRI toIri(String value) {
         ValueFactory factory = SimpleValueFactory.getInstance();
         return factory.createIRI(value);
+    }
+    
+    public static Node IriToNode(String Iri) {
+        Resource subjRes = new Resource("<" + Iri + ">", true); // true means you are supplying proper N-Triples RDF terms that do not need to be processed
+        Resource predRes = new Resource("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>", true);
+        BNode bn = new BNode("_:bnodeId", true);
+        Node[] triple = new Node[]{subjRes, predRes, bn}; // yields <http://example.org/123> <http://example.org/123> _:bnodeId
+        return triple[0];
     }
 }
