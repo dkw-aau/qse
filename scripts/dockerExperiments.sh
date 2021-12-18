d34d92b33dcedba98ae319e47138067a0b11b9c1
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2021-12-18 03:59:09 +0100

    Logs path changed

--- scripts/dockerExperiments.sh
+++ scripts/dockerExperiments.sh
@@ -3,20 +3,21 @@
 #docker build . -t shacl:latest
 #docker run -it --name testContainer alpine:latest ash
 
-containerName=shacl_wikidata_maxCard
+container=shacl_wikidata_maxCard
 docker ps
 
-# shellcheck disable=SC2046
-if [ $( docker ps -a -f name=containerName | wc -l ) -eq 2 ]; then
-  echo "${containerName} exists"
+
+
+if [ $( docker ps -a -f name=${container} | wc -l ) -eq 2 ]; then
+  echo "${container} exists"
 else
-  echo "${containerName} does not exist"
+  echo "${container} does not exist"
 fi
 
 
-output=$( docker ps -a -f name=containerName | grep containerName 2> /dev/null )
+output=$( docker ps -a -f name=${container} | grep ${container} 2> /dev/null )
 if [[ ! -z ${output} ]]; then
-  echo "A container with a name: ${containerName} exists and has status: $( echo ${output} | awk '{ print $7 }' )"
+  echo "A container with a name: ${container} exists and has status: $( echo ${output} | awk '{ print $7 }' )"
 else
-  echo "Container with a name: ${containerName} does not exist"
+  echo "Container with a name: ${container} does not exist"
 fi
