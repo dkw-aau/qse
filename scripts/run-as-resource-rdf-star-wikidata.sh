448969f91da8c50a2a71dadd64a65e9df58f8bba
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-01-11 13:38:42 +0100

    RDF-Star and Reification Wikidata scripts

---
+++ scripts/run-as-resource-rdf-star-wikidata.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+### Build Docker Image
+cd ..
+docker build . -t shacl:QSE-LATEST-RDF-STAR-WIKIDATA
+
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------"
+### Clear Cache
+echo "Clearing cache"
+sync; echo 1 > /proc/sys/vm/drop_caches
+
+container=QSE_RDFSTAR_WIKIDATA_wo_maxCard
+
+echo "About to run docker container: ${container}"
+
+docker run -m 750GB -d --name QSE_RDFSTAR_WIKIDATA_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx700g" --mount type=bind,source=/user/cs.aau.dk/iq26og/data/,target=/app/data --mount type=bind,source=/user/cs.aau.dk/iq26og/git/shacl/,target=/app/local shacl:QSE-LATEST-RDF-STAR-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+
+docker ps
+
+# Get the status of the current docker container
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}"
+
+### Keep it in sleep for 1 minutes while this container is running
+while :
+do
+  status=$(docker container inspect -f '{{.State.Status}}' $container)
+  if [ $status == "exited" ]; then
+    break
+  fi
+  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
+  echo "Sleeping for 1 minutes : $(date +%T)"
+  sleep 1m
+done
+
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}" ### Container exited
+
+
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------"
+### Clear Cache
+echo "Clearing cache"
+sync; echo 1 > /proc/sys/vm/drop_caches
+
+
+container=QSE_RDFSTAR_WIKIDATA_wd_maxCard
+
+echo "About to run docker container: ${container}"
+
+docker run -m 750GB -d --name QSE_RDFSTAR_WIKIDATA_wd_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx700g" --mount type=bind,source=/user/cs.aau.dk/iq26og/data/,target=/app/data --mount type=bind,source=/user/cs.aau.dk/iq26og/git/shacl/,target=/app/local shacl:QSE-LATEST-RDF-STAR-WIKIDATA /app/local/config/wd-max-card/wikiDataConfig.properties
+
+docker ps
+
+# Get the status of the current docker container
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}"
+
+### Keep it in sleep for 1 minutes while this container is running
+while :
+do
+  status=$(docker container inspect -f '{{.State.Status}}' $container)
+  if [ $status == "exited" ]; then
+    break
+  fi
+  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
+  echo "Sleeping for 1 minutes : $(date +%T)"
+  sleep 1m
+done
+
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}" ### Container exited
+
