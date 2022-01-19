32be73946b7f9f27660300070a0be3518c63b82a
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-01-19 23:34:20 +0100

    Main file change

--- src/main/java/cs/Main.java
+++ src/main/java/cs/Main.java
@@ -54,11 +54,11 @@ public class Main {
                 //Parser parser = new Parser(datasetPath, numberOfClasses, numberOfInstances, Constants.INSTANCE_OF);
                 //parser.run();
                 
-                RandomSamplingParser rsp = new RandomSamplingParser(datasetPath, numberOfClasses, numberOfInstances, Constants.INSTANCE_OF, entitySamplingThreshold);
-                rsp.run();
+                //RandomSamplingParser rsp = new RandomSamplingParser(datasetPath, numberOfClasses, numberOfInstances, Constants.INSTANCE_OF, entitySamplingThreshold);
+                //rsp.run();
                 
-                //ReservoirSamplingParser reservoirSamplingParser = new ReservoirSamplingParser(datasetPath, numberOfClasses, numberOfInstances, Constants.INSTANCE_OF, entitySamplingThreshold);
-                //reservoirSamplingParser.run();
+                ReservoirSamplingParser reservoirSamplingParser = new ReservoirSamplingParser(datasetPath, numberOfClasses, numberOfInstances, Constants.INSTANCE_OF, entitySamplingThreshold);
+                reservoirSamplingParser.run();
             }
             
         } catch (Exception e) {
