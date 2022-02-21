b43e544c467e14826d771ec2f93fcd08b203b929
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-02-21 16:44:22 +0100

    WikiData Script for Trie Testing

--- scripts/simple.sh
+++ scripts/simple.sh
@@ -2,18 +2,54 @@
 
 ### Build Docker Image
 cd ..
-docker build . -t shacl:QSE-MemoryMeasuring-WIKIDATA
+docker build . -t shacl:QSE-MemoryMeasuringTrie-WIKIDATA
 
-echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 1 - 50GB/45gb ------------------"
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 1 - 30GB/25gb ------------------"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_WIKIDATA_MemoryMeasuring-1_wo_maxCard
+container=QSE_WIKIDATA_MemoryMeasuringTrie-1_wo_maxCard
 
 echo "About to run docker container: ${container}"
 
-docker run -m 50GB -d --name QSE_WIKIDATA_MemoryMeasuring-1_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx45g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuring-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+docker run -m 30GB -d --name QSE_WIKIDATA_MemoryMeasuringTrie-1_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx25g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuringTrie-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+
+docker ps
+
+# Get the status of the current docker container
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}"
+
+### Keep it in sleep for 1 minutes while this container is running
+while :
+do
+  status=$(docker container inspect -f '{{.State.Status}}' $container)
+  if [ $status == "exited" ]; then
+    break
+  fi
+  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
+  echo "Sleeping for 1 minutes : $(date +%T)"
+  sleep 1m
+done
+
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}" ### Container exited
+
+
+
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 2 - 40GB/35gb ------------------"
+### Clear Cache
+echo "Clearing cache"
+sync; echo 1 > /proc/sys/vm/drop_caches
+
+container=QSE_WIKIDATA_MemoryMeasuringTrie-2_wo_maxCard
+
+echo "About to run docker container: ${container}"
+
+docker run -m 40GB -d --name QSE_WIKIDATA_MemoryMeasuringTrie-2_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx35g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuringTrie-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
 
 docker ps
 
@@ -43,16 +79,16 @@ echo "Status of the ${container} is ${status}" ### Container exited
 
 
 
-echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 2 - 60GB/55gb ------------------"
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 3 - 50GB/45gb ------------------"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_WIKIDATA_MemoryMeasuring-2_wo_maxCard
+container=QSE_WIKIDATA_MemoryMeasuringTrie-3_wo_maxCard
 
 echo "About to run docker container: ${container}"
 
-docker run -m 60GB -d --name QSE_WIKIDATA_MemoryMeasuring-2_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx55g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuring-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+docker run -m 50GB -d --name QSE_WIKIDATA_MemoryMeasuringTrie-3_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx45g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuringTrie-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
 
 docker ps
 
@@ -82,16 +118,16 @@ echo "Status of the ${container} is ${status}" ### Container exited
 
 
 
-echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 3 - 70GB/65gb ------------------"
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 4 - 60GB/55gb ------------------"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_WIKIDATA_MemoryMeasuring-3_wo_maxCard
+container=QSE_WIKIDATA_MemoryMeasuringTrie-4_wo_maxCard
 
 echo "About to run docker container: ${container}"
 
-docker run -m 70GB -d --name QSE_WIKIDATA_MemoryMeasuring-3_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx65g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuring-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+docker run -m 60GB -d --name QSE_WIKIDATA_MemoryMeasuringTrie-4_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx55g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuringTrie-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
 
 docker ps
 
@@ -121,16 +157,16 @@ echo "Status of the ${container} is ${status}" ### Container exited
 
 
 
-echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 4 - 80GB/75gb ------------------"
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 5 - 70GB/65gb ------------------"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_WIKIDATA_MemoryMeasuring-4_wo_maxCard
+container=QSE_WIKIDATA_MemoryMeasuringTrie-5_wo_maxCard
 
 echo "About to run docker container: ${container}"
 
-docker run -m 80GB -d --name QSE_WIKIDATA_MemoryMeasuring-4_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx75g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuring-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+docker run -m 70GB -d --name QSE_WIKIDATA_MemoryMeasuringTrie-5_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx65g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuringTrie-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
 
 docker ps
 
@@ -160,55 +196,16 @@ echo "Status of the ${container} is ${status}" ### Container exited
 
 
 
-echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 5 - 90GB/85gb ------------------"
+echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 6 - 80GB/75gb ------------------"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_WIKIDATA_MemoryMeasuring-5_wo_maxCard
+container=QSE_WIKIDATA_MemoryMeasuringTrie-6_wo_maxCard
 
 echo "About to run docker container: ${container}"
 
-docker run -m 90GB -d --name QSE_WIKIDATA_MemoryMeasuring-5_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx85g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuring-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
-
-docker ps
-
-# Get the status of the current docker container
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}"
-
-### Keep it in sleep for 1 minutes while this container is running
-while :
-do
-  status=$(docker container inspect -f '{{.State.Status}}' $container)
-  if [ $status == "exited" ]; then
-    break
-  fi
-  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
-  echo "Sleeping for 1 minutes : $(date +%T)"
-  sleep 1m
-done
-
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}" ### Container exited
-
-
-
-
-
-
-echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS Measuring Memory 6 - 100GB/95gb ------------------"
-### Clear Cache
-echo "Clearing cache"
-sync; echo 1 > /proc/sys/vm/drop_caches
-
-container=QSE_WIKIDATA_MemoryMeasuring-6_wo_maxCard
-
-echo "About to run docker container: ${container}"
-
-docker run -m 100GB -d --name QSE_WIKIDATA_MemoryMeasuring-6_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx95g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuring-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+docker run -m 80GB -d --name QSE_WIKIDATA_MemoryMeasuringTrie-6_wo_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx75g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-MemoryMeasuringTrie-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
 
 docker ps
 
