33b1b8f2e4a6fa631ec8cc8f749bf92bed8dc414
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-02-24 15:55:49 +0100

    run-wikidata script

--- scripts/run-wikidata-reservoir-sampling.sh
+++ scripts/run-wikidata-reservoir-sampling.sh
@@ -2,18 +2,18 @@
 
 ### Build Docker Image
 cd ..
-docker build . -t shacl:QSE-ReservoirSampling-WIKIDATA
+docker build . -t shacl:QSE-DynamicReservoirSampling-WIKIDATA
 
 echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_WIKIDATA_ReservoirSampling_wo_maxCard_Increasing20to300_bullyApproach
+container=QSE_WIKIDATA_DynamicBullyResSampling_WoMax_1_300
 
 echo "About to run docker container: ${container}"
 
-docker run -m 150GB -d --name QSE_WIKIDATA_ReservoirSampling_wo_maxCard_Increasing20to300_bullyApproach -e "JAVA_TOOL_OPTIONS=-Xmx125g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-ReservoirSampling-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
+docker run -m 150GB -d --name QSE_WIKIDATA_DynamicBullyResSampling_WoMax_1_300 -e "JAVA_TOOL_OPTIONS=-Xmx125g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-DynamicReservoirSampling-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
 
 docker ps
 
@@ -49,7 +49,7 @@ echo "Status of the ${container} is ${status}" ### Container exited
 #
 #echo "About to run docker container: ${container}"
 #
-#docker run -m 200GB -d --name QSE_WIKIDATA_ReservoirSampling_wd_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx150g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-ReservoirSampling-WIKIDATA /app/local/config/wd-max-card/wikiDataConfig.properties
+#docker run -m 200GB -d --name QSE_WIKIDATA_ReservoirSampling_wd_maxCard -e "JAVA_TOOL_OPTIONS=-Xmx150g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local shacl:QSE-DynamicReservoirSampling-WIKIDATA /app/local/config/wd-max-card/wikiDataConfig.properties
 #
 #docker ps
 #
