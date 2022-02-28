c8ac4ecf58eb6e78f1dafdfc228cf45dd29495bd
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-02-28 03:46:35 +0100

    wikidata big server script

--- scripts/wikidata-big-server.sh
+++ scripts/wikidata-big-server.sh
@@ -2,57 +2,18 @@
 
 ### Build Docker Image
 cd ..
-docker build . -t shacl:QSE-Full-WIKIDATA
+docker build . -t shacl:QSE-Full-WIKIDATA-Default
 
 echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY FULL ------------------"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_WIKIDATA_full_wo_maxCard_reverse_no_prop_pruning
+container=QSE_WIKIDATA_full_wo_maxCard_default
 
 echo "About to run docker container: ${container}"
 
-docker run -m 750GB -d --name QSE_WIKIDATA_full_wo_maxCard_reverse_no_prop_pruning -e "JAVA_TOOL_OPTIONS=-Xmx550g" --mount type=bind,source=/user/cs.aau.dk/iq26og/data/,target=/app/data --mount type=bind,source=/user/cs.aau.dk/iq26og/git/shacl/,target=/app/local shacl:QSE-Full-WIKIDATA /app/local/config/wo-max-card/wikiDataConfig.properties
-
-docker ps
-
-# Get the status of the current docker container
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}"
-
-### Keep it in sleep for 1 minutes while this container is running
-while :
-do
-  status=$(docker container inspect -f '{{.State.Status}}' $container)
-  if [ $status == "exited" ]; then
-    break
-  fi
-  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
-  echo "Sleeping for 1 minutes : $(date +%T)"
-  sleep 1m
-done
-
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}" ### Container exited
-
-
-git pull
-
-docker build . -t shacl:QSE-Full-WIKIDATA-Reverse
-
-echo "------------------ WIKIDATA WITHOUT MAX CARDINALITY FULL again ------------------"
-### Clear Cache
-echo "Clearing cache"
-sync; echo 1 > /proc/sys/vm/drop_caches
-
-container=QSE_WIKIDATA_full_wo_maxCard_reverse_with_prop_pruning
-
-echo "About to run docker container: ${container}"
-
-docker run -m 750GB -d --name QSE_WIKIDATA_full_wo_maxCard_reverse_with_prop_pruning -e "JAVA_TOOL_OPTIONS=-Xmx550g" --mount type=bind,source=/user/cs.aau.dk/iq26og/data/,target=/app/data --mount type=bind,source=/user/cs.aau.dk/iq26og/git/shacl/,target=/app/local shacl:QSE-Full-WIKIDATA-Reverse /app/local/config/wo-max-card/wikiDataConfig.properties
+docker run -m 750GB -d --name QSE_WIKIDATA_full_wo_maxCard_default -e "JAVA_TOOL_OPTIONS=-Xmx550g" --mount type=bind,source=/user/cs.aau.dk/iq26og/data/,target=/app/data --mount type=bind,source=/user/cs.aau.dk/iq26og/git/shacl/,target=/app/local shacl:QSE-Full-WIKIDATA-Default /app/local/config/wo-max-card/wikiDataConfig.properties
 
 docker ps
 
