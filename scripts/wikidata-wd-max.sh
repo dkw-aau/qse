7fa29c1771eaf4e69e713549bc1234002b01f7ad
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-06-12 14:11:54 +0200

    QSE-Exact Big Server
    - script to run wikidata with max cardinality constraints.

---
+++ scripts/wikidata-wd-max.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+cd ..
+
+### Build Docker Image
+image=shacl:QSE-Exact
+docker build . -t $image
+
+echo "------------------ A3: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------"
+### Clear Cache
+echo "Clearing cache"
+sync; echo 1 > /proc/sys/vm/drop_caches
+
+container=QSE_WIKIDATA_Exact_With_Max_Cardinality
+
+echo "About to run docker container: ${container}"
+
+docker run -m 750GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx550g" --mount type=bind,source=/user/cs.aau.dk/iq26og/data/,target=/app/data --mount type=bind,source=/user/cs.aau.dk/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wd-max-card/wikiDataConfig.properties
+
+docker ps
+
+# Get the status of the current docker container
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}"
+
+### Keep it in sleep for 1 minutes while this container is running
+while :
+do
+  status=$(docker container inspect -f '{{.State.Status}}' $container)
+  if [ $status == "exited" ]; then
+    break
+  fi
+  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
+  echo "Sleeping for 1 minutes : $(date +%T)"
+  sleep 1m
+done
+
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}" ### Container exited
+
