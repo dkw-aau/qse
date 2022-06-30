8f715715a5b962744c1ff3b24e7b7ff772af26ba
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-06-30 15:22:53 +0200

    mem

--- scripts/sampling-parameters-sp-10-rs-20-100.sh
+++ scripts/sampling-parameters-sp-10-rs-20-100.sh
@@ -122,18 +122,92 @@ docker build . -t $image
 #docker logs $container &> "docker_logs_${container}.txt"
 
 
-#### sp-100-rs-500
+#
+#
+#echo "------------------ wikiData-sp-75-rs-5000.properties: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------ xmx 32"
+#### Clear Cache
+#echo "Clearing cache"
+#sync; echo 1 > /proc/sys/vm/drop_caches
+#
+#container=QSE_Approximate_wikiData_30_June-sp-75-rs-5000_xmx_32g
+#
+#echo "About to run docker container: ${container}"
+#
+#docker run -m 100GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx32g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local $image /app/local/config/wo-max-card/wiki/wikiData-sp-75-rs-5000.properties
+#
+#docker ps
+#
+## Get the status of the current docker container
+#status=$(docker container inspect -f '{{.State.Status}}' $container)
+#
+#echo "Status of the ${container} is ${status}"
+#
+#### Keep it in sleep for 1 minutes while this container is running
+#while :
+#do
+#  status=$(docker container inspect -f '{{.State.Status}}' $container)
+#  if [ $status == "exited" ]; then
+#    break
+#  fi
+#  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
+#  echo "Sleeping for 1 minutes : $(date +%T)"
+#  sleep 1m
+#done
+#
+#status=$(docker container inspect -f '{{.State.Status}}' $container)
+#
+#echo "Status of the ${container} is ${status}" ### Container exited
+#docker logs $container &> "docker_logs_${container}.txt"
+#
+#
+#
+#
+#echo "------------------ wikiData-sp-100-rs-500.properties: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------ xmx 32"
+#### Clear Cache
+#echo "Clearing cache"
+#sync; echo 1 > /proc/sys/vm/drop_caches
+#
+#container=QSE_Approximate_wikiData_30_June-sp-100-rs-500_xmx_32g
+#
+#echo "About to run docker container: ${container}"
+#
+#docker run -m 100GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx32g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local $image /app/local/config/wo-max-card/wiki/wikiData-sp-100-rs-500.properties
+#
+#docker ps
+#
+## Get the status of the current docker container
+#status=$(docker container inspect -f '{{.State.Status}}' $container)
+#
+#echo "Status of the ${container} is ${status}"
+#
+#### Keep it in sleep for 1 minutes while this container is running
+#while :
+#do
+#  status=$(docker container inspect -f '{{.State.Status}}' $container)
+#  if [ $status == "exited" ]; then
+#    break
+#  fi
+#  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
+#  echo "Sleeping for 1 minutes : $(date +%T)"
+#  sleep 1m
+#done
+#
+#status=$(docker container inspect -f '{{.State.Status}}' $container)
+#
+#echo "Status of the ${container} is ${status}" ### Container exited
+#docker logs $container &> "docker_logs_${container}.txt"
+#
 
-echo "------------------ wikiData-sp-75-rs-5000.properties: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------ xmx 32"
+echo "------------------ wikiData-sp-100-rs-5000.properties: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------ xmx 50"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_Approximate_wikiData_30_June-sp-75-rs-5000_xmx_32g
+container=QSE_Approximate_wikiData_30_June-sp-100-rs-5000_xmx_50g
 
 echo "About to run docker container: ${container}"
 
-docker run -m 100GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx32g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local $image /app/local/config/wo-max-card/wiki/wikiData-sp-75-rs-5000.properties
+docker run -m 100GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx50g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local $image /app/local/config/wo-max-card/wiki/wikiData-sp-100-rs-5000.properties
 
 docker ps
 
@@ -160,54 +234,16 @@ echo "Status of the ${container} is ${status}" ### Container exited
 docker logs $container &> "docker_logs_${container}.txt"
 
 
-
-
-echo "------------------ wikiData-sp-100-rs-500.properties: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------ xmx 32"
+echo "------------------ wikiData-sp-100-rs-5000.properties: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------ xmx 50"
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QSE_Approximate_wikiData_30_June-sp-100-rs-500_xmx_32g
+container=QSE_Approximate_wikiData_30_June-sp-100-rs-5000_xmx_40g
 
 echo "About to run docker container: ${container}"
 
-docker run -m 100GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx32g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local $image /app/local/config/wo-max-card/wiki/wikiData-sp-100-rs-500.properties
-
-docker ps
-
-# Get the status of the current docker container
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}"
-
-### Keep it in sleep for 1 minutes while this container is running
-while :
-do
-  status=$(docker container inspect -f '{{.State.Status}}' $container)
-  if [ $status == "exited" ]; then
-    break
-  fi
-  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
-  echo "Sleeping for 1 minutes : $(date +%T)"
-  sleep 1m
-done
-
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}" ### Container exited
-docker logs $container &> "docker_logs_${container}.txt"
-
-
-echo "------------------ wikiData-sp-100-rs-5000.properties: WIKIDATA WITHOUT MAX CARDINALITY CONSTRAINTS ------------------ xmx 32"
-### Clear Cache
-echo "Clearing cache"
-sync; echo 1 > /proc/sys/vm/drop_caches
-
-container=QSE_Approximate_wikiData_30_June-sp-100-rs-5000_xmx_32g
-
-echo "About to run docker container: ${container}"
-
-docker run -m 100GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx32g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local $image /app/local/config/wo-max-card/wiki/wikiData-sp-100-rs-5000.properties
+docker run -m 100GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx40g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git/shacl/,target=/app/local $image /app/local/config/wo-max-card/wiki/wikiData-sp-100-rs-5000.properties
 
 docker ps
 
