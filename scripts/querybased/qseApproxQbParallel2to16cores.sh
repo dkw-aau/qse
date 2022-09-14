8513749fcfd7ad114ba3bae416b6259bb7fd420f
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-09-14 22:14:23 +0200

    Testing QSE Approx Parallel with less memory

--- scripts/querybased/qseApproxQbParallel2to16cores.sh
+++ scripts/querybased/qseApproxQbParallel2to16cores.sh
@@ -15,7 +15,7 @@ container=QseApproxQbPar_1k_100_wikiDataConfig2
 
 echo "About to run docker container: ${container}"
 
-docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx96g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig2.properties
+docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx64g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig2.properties
 
 docker ps
 
@@ -40,6 +40,7 @@ status=$(docker container inspect -f '{{.State.Status}}' $container)
 
 echo "Status of the ${container} is ${status}" ### Container exited
 
+docker logs ${container} &> ${container}.logs
 
 
 
@@ -53,7 +54,7 @@ container=QseApproxQbPar_1k_100_wikiDataConfig4
 
 echo "About to run docker container: ${container}"
 
-docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx96g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig4.properties
+docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx64g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig4.properties
 
 docker ps
 
@@ -78,7 +79,7 @@ status=$(docker container inspect -f '{{.State.Status}}' $container)
 
 echo "Status of the ${container} is ${status}" ### Container exited
 
-
+docker logs ${container} &> ${container}.logs
 
 
 
@@ -92,7 +93,7 @@ container=QseApproxQbPar_1k_100_wikiDataConfig8
 
 echo "About to run docker container: ${container}"
 
-docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx96g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig8.properties
+docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx64g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig8.properties
 
 docker ps
 
@@ -117,7 +118,7 @@ status=$(docker container inspect -f '{{.State.Status}}' $container)
 
 echo "Status of the ${container} is ${status}" ### Container exited
 
-
+docker logs ${container} &> ${container}.logs
 
 
 
@@ -131,7 +132,7 @@ container=QseApproxQbPar_1k_100_wikiDataConfig16
 
 echo "About to run docker container: ${container}"
 
-docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx96g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig16.properties
+docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx64g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig16.properties
 
 docker ps
 
@@ -156,7 +157,7 @@ status=$(docker container inspect -f '{{.State.Status}}' $container)
 
 echo "Status of the ${container} is ${status}" ### Container exited
 
-
+docker logs ${container} &> ${container}.logs
 
 echo "------------------  wikiDataConfig1 ------------------"
 
@@ -168,7 +169,7 @@ container=QseApproxQbPar_1k_100_wikiDataConfig1
 
 echo "About to run docker container: ${container}"
 
-docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx96g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig1.properties
+docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx64g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/qse-endpoint/wikiDataConfig1.properties
 
 docker ps
 
@@ -192,3 +193,5 @@ done
 status=$(docker container inspect -f '{{.State.Status}}' $container)
 
 echo "Status of the ${container} is ${status}" ### Container exited
+
+docker logs ${container} &> ${container}.logs
