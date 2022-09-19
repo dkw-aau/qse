ddbbca677973631d0dc50e48602a583bd1a5c63e
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-09-19 17:48:55 +0200

    5k and 100 percent wikidata 21

--- scripts/querybased/qseApproxQbParallel2to16coresWdt21.sh
+++ scripts/querybased/qseApproxQbParallel2to16coresWdt21.sh
@@ -5,13 +5,51 @@ cd ../../
 image=shacl:QSE-ApproxQbParallel
 docker build . -t $image
 
+echo "------------------  wikiDataConfig1 ------------------"
+
+### Clear Cache
+echo "Clearing cache"
+sync; echo 1 > /proc/sys/vm/drop_caches
+
+container=QseApproxQbPar_5k_100_wikiDataConfig1
+
+echo "About to run docker container: ${container}"
+
+docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx64g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/wdt21-qse-endpoint/wikiDataConfig1.properties
+
+docker ps
+
+# Get the status of the current docker container
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}"
+
+### Keep it in sleep for 1 minutes while this container is running
+while :
+do
+  status=$(docker container inspect -f '{{.State.Status}}' $container)
+  if [ $status == "exited" ]; then
+    break
+  fi
+  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
+  echo "Sleeping for 1 minutes : $(date +%T)"
+  sleep 1m
+done
+
+status=$(docker container inspect -f '{{.State.Status}}' $container)
+
+echo "Status of the ${container} is ${status}" ### Container exited
+
+docker logs ${container} &> ${container}.logs
+
+
 echo "------------------  wikiDataConfig2 ------------------"
 
 ### Clear Cache
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QseApproxQbPar_1k_100_wikiDataConfig2
+container=QseApproxQbPar_5k_100_wikiDataConfig2
 
 echo "About to run docker container: ${container}"
 
@@ -50,7 +88,7 @@ echo "------------------  wikiDataConfig4 ------------------"
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QseApproxQbPar_1k_100_wikiDataConfig4
+container=QseApproxQbPar_5k_100_wikiDataConfig4
 
 echo "About to run docker container: ${container}"
 
@@ -89,7 +127,7 @@ echo "------------------  wikiDataConfig8 ------------------"
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QseApproxQbPar_1k_100_wikiDataConfig8
+container=QseApproxQbPar_5k_100_wikiDataConfig8
 
 echo "About to run docker container: ${container}"
 
@@ -128,7 +166,7 @@ echo "------------------  wikiDataConfig16 ------------------"
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=QseApproxQbPar_1k_100_wikiDataConfig16
+container=QseApproxQbPar_5k_100_wikiDataConfig16
 
 echo "About to run docker container: ${container}"
 
@@ -159,39 +197,3 @@ echo "Status of the ${container} is ${status}" ### Container exited
 
 docker logs ${container} &> ${container}.logs
 
-echo "------------------  wikiDataConfig1 ------------------"
-
-### Clear Cache
-echo "Clearing cache"
-sync; echo 1 > /proc/sys/vm/drop_caches
-
-container=QseApproxQbPar_1k_100_wikiDataConfig1
-
-echo "About to run docker container: ${container}"
-
-docker run -m 128GB -d --name $container -e "JAVA_TOOL_OPTIONS=-Xmx64g" --mount type=bind,source=/srv/data/iq26og/data/,target=/app/data --mount type=bind,source=/srv/data/iq26og/git3/shacl/,target=/app/local $image /app/local/config/wo-max-card/wdt21-qse-endpoint/wikiDataConfig1.properties
-
-docker ps
-
-# Get the status of the current docker container
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}"
-
-### Keep it in sleep for 1 minutes while this container is running
-while :
-do
-  status=$(docker container inspect -f '{{.State.Status}}' $container)
-  if [ $status == "exited" ]; then
-    break
-  fi
-  docker stats --no-stream | cat >>   "${container}-Docker-Stats.csv"
-  echo "Sleeping for 1 minutes : $(date +%T)"
-  sleep 1m
-done
-
-status=$(docker container inspect -f '{{.State.Status}}' $container)
-
-echo "Status of the ${container} is ${status}" ### Container exited
-
-docker logs ${container} &> ${container}.logs
