66ba86c686d9f3ef85d8588314567f0eb55b81cb
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2022-10-08 13:11:23 +0200

    batch size 1000

--- src/main/java/cs/qse/querybased/sampling/QbSampling.java
+++ src/main/java/cs/qse/querybased/sampling/QbSampling.java
@@ -185,8 +185,8 @@ public class QbSampling {
         }
         
         typesToEntities.forEach((types, entities) -> {
-            if (entities.size() > 750) {
-                List<List<String>> subEntities = Lists.partition(entities, 500);
+            if (entities.size() > 1000) {
+                List<List<String>> subEntities = Lists.partition(entities, 1000);
                 subEntities.forEach(listOfSubEntities -> {
                     String batchQuery = buildBatchQuery(types, listOfSubEntities, typePredicate); // ?p ?o are binding variables
                     iterateOverEntityTriples(types, batchQuery);
